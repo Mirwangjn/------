81107a5b2401b47e4a7402db71aa52850e20389a
Wang <2041@qq.com>
2023-07-10 17:24:55 +0800

    third commit interceptors

--- js/axios源码.js
+++ js/axios源码.js
@@ -1,10 +1,32 @@
 function Axios(instanceConfig){
     this.default = instanceConfig;
+    /*
+        拦截器内部实现原理为：将拦截器的函数存储起来，然后遍历添加到chains数组上
+        请求拦截器使用unshift()添加到chains数组前面;而响应拦截器使用push添加到后面
+        所以存在两个请求拦截器时，会先执行第2个，然后在执行第一个
+        [two(config),two(error),one(config),one(error),dispatchRequest,undefined，。。。]
+        源码为：chain.unshift(interceptor.fulfilled,interceptor.rejected)
+        while(chain.length){promise = promise.then(chain.shift(),chain.shift())}
+        这是undefined有着极大的作用。如果请求的promise状态为rejected，在执行到undefined时
+        promise会异常穿透(你不写内部帮你生成)
+    */
+     
     this.interceptors = {
-        request:{},
-        response:{}
+        request:new InterceptorManager(),
+        response:new InterceptorManager()
     }
 };
+//实现拦截器
+function InterceptorManager(){
+    this.handlers = [];
+}
+InterceptorManager.prototype.use = function(fulfilled,rejected){
+    //use的作用是将参数添加到handlers身上，然后遍历添加到chain中，然后在遍历运行chain
+    this.handlers.push({
+        fulfilled,
+        rejected
+    })
+}
 function dispatchRequest(config){
     //这一步是用来对数据进行整理
     return xrlAdepter(config).then((response) =>{
@@ -46,10 +68,28 @@ function xrlAdepter(config){
 //通过request发送请求
 Axios.prototype.request = function(config){
     // console.log("request请求"+ config.method);
+    //传入的参数在先，修改在后
     let promise = Promise.resolve(config);
+    
     let chains = [dispatchRequest,undefined];
-    let result = promise.then(chains[0],chains[1]);
-    return result
+    //添加请求拦截器函数
+    this.interceptors.request.handlers.forEach(manyFn =>{
+       chains.unshift(manyFn.fulfilled,manyFn.rejected);
+    });
+    //添加响应拦截器函数
+    this.interceptors.response.handlers.forEach(manyFn =>{
+        chains.push(manyFn.fulfilled,manyFn.rejected);
+    })
+    // console.log(chains);
+    // 拦截器函数添加完成，进行遍历
+    let i = 0;
+    while(chains.length > i){
+        // promise.then(chains.shift(),chains.shift());//老源码写法
+        promise = promise.then(chains[i++],chains[i++]);//新源码写法
+    }
+
+    // let result = promise.then(chains[0],chains[1]);
+    return promise;
 };
 // get和post内部是调用了request请求
 Axios.prototype.get = function(config){
@@ -81,5 +121,5 @@ function createInstance(defaultConfig){
     return instance;
 };
 //赋值
-const axios = createInstance();//传入值为axios默认的配置对象
+const axios = createInstance({method:"get"});//传入值为axios默认的配置对象
 // axios.request({method:"post"})
